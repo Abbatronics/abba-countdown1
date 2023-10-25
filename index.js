ee4674ede137b9af39b25b69bba39d7402894218
Abbatronics <125681774+Abbatronics@users.noreply.github.com>
2023-10-25 20:00:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,23 @@ btnEl.addEventListener("click", () => {
         `;
   }
 });
+const menuEl = document.querySelector(".menu");
+const menuTextEl = document.querySelector(".menu p");
+const socialListsEl = document.querySelector(".social-lists");
+const liEls = document.querySelectorAll(".social-lists li");
 
+menuEl.addEventListener("click", () => {
+  socialListsEl.classList.toggle("hide");
+  menuEl.classList.toggle("rotate");
+});
+
+liEls.forEach((liEl) => {
+  liEl.addEventListener("click", () => {
+    menuTextEl.innerHTML = liEl.innerHTML;
+    socialListsEl.classList.add("hide");
+    menuEl.classList.toggle("rotate");
+  });
+});
 function removeActive() {
   ratingEls.forEach((ratingEl) => {
     ratingEl.classList.remove("active");
